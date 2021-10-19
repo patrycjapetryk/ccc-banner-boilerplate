886f6329c1f2e3d9320cd26816965efd06870581
patrycjapetryk <patrycja.petryk@gmail.com>
2021-10-19 11:26:21 +0200

    Add infoboxes and listing

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,8 @@ module.exports = {
   output: {
     filename: 'js/[name].[hash:8].js',
     path: __dirname + '/dist/',
-    assetModuleFilename: 'images/[name].[hash:8][ext][query]',
+    // assetModuleFilename: 'images/[name].[hash:8][ext][query]',
+    assetModuleFilename: 'images/[name][ext][query]',
   },
   module: {
     rules: [
@@ -61,7 +62,8 @@ module.exports = {
         test: /\.mp4|webm$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'video/[name].[hash:8][ext][query]',
+          // filename: 'video/[name].[hash:8][ext][query]',
+          filename: 'video/[name][ext][query]',
         },
       },
     ],
@@ -88,20 +90,20 @@ module.exports = {
     //   filename: 'index.html',
     //   minify: false,
     // }),
-    // new HtmlWebpackPlugin({
-    //   template: './src/pages/infobox-1.html',
-    //   inject: 'body',
-    //   chunks: ['index'],
-    //   filename: 'index.html',
-    //   minify: false,
-    // }),
     new HtmlWebpackPlugin({
-      template: './src/pages/doublebox.html',
+      template: './src/pages/infobox-1.html',
       inject: 'body',
       chunks: ['index'],
       filename: 'index.html',
       minify: false,
     }),
+    // new HtmlWebpackPlugin({
+    //   template: './src/pages/doublebox.html',
+    //   inject: 'body',
+    //   chunks: ['index'],
+    //   filename: 'index.html',
+    //   minify: false,
+    // }),
     // new HtmlWebpackPlugin({
     //   template: './src/pages/slider-nav.html',
     //   inject: 'body',
