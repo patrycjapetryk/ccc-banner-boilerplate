cf710dd6db9275a45ccf9aceee2592469aece1e7
patrycjapetryk <patrycja.petryk@gmail.com>
2021-10-28 09:39:33 +0200

    Change doubleboxes

--- webpack.common.js
+++ webpack.common.js
@@ -69,13 +69,13 @@ module.exports = {
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/pages/index.html',
-      inject: 'body',
-      chunks: ['index'],
-      filename: 'index.html',
-      minify: false,
-    }),
+    // new HtmlWebpackPlugin({
+    //   template: './src/pages/index.html',
+    //   inject: 'body',
+    //   chunks: ['index'],
+    //   filename: 'index.html',
+    //   minify: false,
+    // }),
     // new HtmlWebpackPlugin({
     //   template: './src/pages/slider.html',
     //   inject: 'body',
@@ -97,13 +97,13 @@ module.exports = {
     //   filename: 'index.html',
     //   minify: false,
     // }),
-    // new HtmlWebpackPlugin({
-    //   template: './src/pages/doublebox.html',
-    //   inject: 'body',
-    //   chunks: ['index'],
-    //   filename: 'index.html',
-    //   minify: false,
-    // }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/doublebox.html',
+      inject: 'body',
+      chunks: ['index'],
+      filename: 'index.html',
+      minify: false,
+    }),
     // new HtmlWebpackPlugin({
     //   template: './src/pages/slider-nav.html',
     //   inject: 'body',
