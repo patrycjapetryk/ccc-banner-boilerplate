880dcc2e7adce0a940c257ff0bd19e98a007c046
patrycjapetryk <patrycja.petryk@gmail.com>
2021-11-29 14:50:31 +0100

    Add click on buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,29 @@
 import '../scss/main.scss';
 
-// console.log('Hello :)');
+const buttons = document.querySelectorAll('.festive-button--js');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    dataLayer.push({
+      event: 'gaevent',
+      category: 'banner_sg_click',
+      action: 'FestiveSeason',
+      pageType: 'home',
+      label: 'dbb click',
+    });
+    dataLayer.push({
+      event: 'gaevent',
+      category: 'banner_sg_click',
+      action: 'FestiveSeason',
+      pageType: 'home',
+      label: 'doublebox_left click',
+    });
+    dataLayer.push({
+      event: 'gaevent',
+      category: 'banner_sg_click',
+      action: 'FestiveSeason',
+      pageType: 'home',
+      label: 'doublebox_right click',
+    });
+  });
+});
