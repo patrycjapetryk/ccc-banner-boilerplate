0504f9310ccb18a1eb3c42290c6f87b3fce4aabd
patrycjapetryk <patrycja.petryk@gmail.com>
2021-11-30 11:01:17 +0100

    Fix button events

--- src/js/index.js
+++ src/js/index.js
@@ -2,28 +2,32 @@ import '../scss/main.scss';
 
 const buttons = document.querySelectorAll('.festive-button--js');
 
-buttons.forEach((button) => {
+buttons.forEach((button, index) => {
   button.addEventListener('click', () => {
-    dataLayer.push({
-      event: 'gaevent',
-      category: 'banner_sg_click',
-      action: 'FestiveSeason',
-      pageType: 'home',
-      label: 'dbb click',
-    });
-    dataLayer.push({
-      event: 'gaevent',
-      category: 'banner_sg_click',
-      action: 'FestiveSeason',
-      pageType: 'home',
-      label: 'doublebox_left click',
-    });
-    dataLayer.push({
-      event: 'gaevent',
-      category: 'banner_sg_click',
-      action: 'FestiveSeason',
-      pageType: 'home',
-      label: 'doublebox_right click',
-    });
+    if (index === 0) {
+      dataLayer.push({
+        event: 'gaevent',
+        category: 'banner_sg_click',
+        action: 'FestiveSeason',
+        pageType: 'home',
+        label: 'dbb click',
+      });
+    } else if (index === 1) {
+      dataLayer.push({
+        event: 'gaevent',
+        category: 'banner_sg_click',
+        action: 'FestiveSeason',
+        pageType: 'home',
+        label: 'doublebox_left click',
+      });
+    } else if (index === 2) {
+      dataLayer.push({
+        event: 'gaevent',
+        category: 'banner_sg_click',
+        action: 'FestiveSeason',
+        pageType: 'home',
+        label: 'doublebox_right click',
+      });
+    }
   });
 });
