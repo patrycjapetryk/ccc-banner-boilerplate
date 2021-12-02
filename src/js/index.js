21297179c0c29dd0448dd6d34216785de0e986a2
patrycjapetryk <patrycja.petryk@gmail.com>
2021-12-02 15:12:31 +0100

    Change font and button styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,37 @@
 import '../scss/main.scss';
 
-const buttons = document.querySelectorAll('.link--js');
+// const buttons = document.querySelectorAll('.link--js');
 
-buttons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    switch (index) {
-      case 0:
-        dataLayer.push({
-          event: 'gaevent',
-          category: 'banner_sg_click',
-          action: 'FestiveSeason',
-          pageType: 'home',
-          label: 'dbb click',
-        });
-        break;
-      case 1:
-        dataLayer.push({
-          event: 'gaevent',
-          category: 'banner_sg_click',
-          action: 'FestiveSeason',
-          pageType: 'home',
-          label: 'doublebox_left click',
-        });
-        break;
-      case 2:
-        dataLayer.push({
-          event: 'gaevent',
-          category: 'banner_sg_click',
-          action: 'FestiveSeason',
-          pageType: 'home',
-          label: 'doublebox_right click',
-        });
-        break;
-    }
-  });
-});
+// buttons.forEach((button, index) => {
+//   button.addEventListener('click', () => {
+//     switch (index) {
+//       case 0:
+//         dataLayer.push({
+//           event: 'gaevent',
+//           category: 'banner_sg_click',
+//           action: 'FestiveSeason',
+//           pageType: 'home',
+//           label: 'dbb click',
+//         });
+//         break;
+//       case 1:
+//         dataLayer.push({
+//           event: 'gaevent',
+//           category: 'banner_sg_click',
+//           action: 'FestiveSeason',
+//           pageType: 'home',
+//           label: 'doublebox_left click',
+//         });
+//         break;
+//       case 2:
+//         dataLayer.push({
+//           event: 'gaevent',
+//           category: 'banner_sg_click',
+//           action: 'FestiveSeason',
+//           pageType: 'home',
+//           label: 'doublebox_right click',
+//         });
+//         break;
+//     }
+//   });
+// });
